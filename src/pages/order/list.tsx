a6d5534b83b25754eda6fe9fd66578add8769022
Sang Le <3lesang@gmail.com>
2025-10-16 14:47:30 +0700

    update order

--- src/pages/order/list.tsx
+++ src/pages/order/list.tsx
@@ -1,6 +1,7 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { Link } from "@tanstack/react-router";
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, ShoppingCartIcon } from "lucide-react";
+import { Activity } from "react";
 import * as timeago from "timeago.js";
 import vi from "timeago.js/lib/lang/vi";
 import TimeAgo from "timeago-react";
@@ -14,6 +15,14 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
+import {
+	Empty,
+	EmptyContent,
+	EmptyDescription,
+	EmptyHeader,
+	EmptyMedia,
+	EmptyTitle,
+} from "@/components/ui/empty";
 import {
 	Item,
 	ItemActions,
@@ -45,45 +54,63 @@ export function ListOrderPage() {
 	return (
 		<main className="bg-neutral-50 h-[calc(100vh-64px)]">
 			<div className="max-w-4xl mx-auto py-8">
-				<Card className="bg-transparent border-0 shadow-none">
-					<CardHeader>
-						<CardTitle>Lịch sử đơn hàng</CardTitle>
-					</CardHeader>
-					<CardContent>
-						<Card className="border-0 shadow-none">
-							<CardHeader>
-								<CardDescription>
-									<Badge variant="secondary">{data.totalItems} đơn hàng</Badge>
-								</CardDescription>
-							</CardHeader>
-							<CardContent className="space-y-2">
-								{data?.items.map((item) => (
-									<Item key={item.id} variant="muted" asChild>
-										<Link to="/order/$id" params={{ id: item.id }}>
-											<ItemHeader>
-												<Badge>{getOrderStatus(item.status)}</Badge>
-											</ItemHeader>
-											<ItemContent>
-												<ItemTitle className="font-bold">
-													{formatVND(item.final_price)}
-												</ItemTitle>
-												<ItemDescription className="space-x-2">
-													<span>{getOrderPayment(item.payment)}</span>
-												</ItemDescription>
-											</ItemContent>
-											<ItemActions>
-												<TimeAgo datetime={item.created} locale="vi" />
-												<Button type="button" variant="ghost" size="icon">
-													<ChevronRightIcon />
-												</Button>
-											</ItemActions>
-										</Link>
-									</Item>
-								))}
-							</CardContent>
-						</Card>
-					</CardContent>
-				</Card>
+				<Activity mode={data.totalItems > 0 ? "visible" : "hidden"}>
+					<Card className="bg-transparent border-0 shadow-none">
+						<CardHeader>
+							<CardTitle>Lịch sử đơn hàng</CardTitle>
+						</CardHeader>
+						<CardContent>
+							<Card className="border-0 shadow-none">
+								<CardHeader>
+									<CardDescription>
+										<Badge variant="secondary">
+											{data.totalItems} đơn hàng
+										</Badge>
+									</CardDescription>
+								</CardHeader>
+								<CardContent className="space-y-2">
+									{data?.items.map((item) => (
+										<Item key={item.id} variant="muted" asChild>
+											<Link to="/order/$id" params={{ id: item.id }}>
+												<ItemHeader>
+													<Badge>{getOrderStatus(item.status)}</Badge>
+												</ItemHeader>
+												<ItemContent>
+													<ItemTitle className="font-bold">
+														{formatVND(item.final_price)}
+													</ItemTitle>
+													<ItemDescription className="space-x-2">
+														<span>{getOrderPayment(item.payment)}</span>
+													</ItemDescription>
+												</ItemContent>
+												<ItemActions>
+													<TimeAgo datetime={item.created} locale="vi" />
+													<Button type="button" variant="ghost" size="icon">
+														<ChevronRightIcon />
+													</Button>
+												</ItemActions>
+											</Link>
+										</Item>
+									))}
+								</CardContent>
+							</Card>
+						</CardContent>
+					</Card>
+				</Activity>
+				<Activity mode={!data.totalItems ? "visible" : "hidden"}>
+					<Empty>
+						<EmptyHeader>
+							<EmptyMedia variant="icon">
+								<ShoppingCartIcon />
+							</EmptyMedia>
+							<EmptyTitle>No data</EmptyTitle>
+							<EmptyDescription>No data found</EmptyDescription>
+						</EmptyHeader>
+						<EmptyContent>
+							<Button>Add data</Button>
+						</EmptyContent>
+					</Empty>
+				</Activity>
 			</div>
 		</main>
 	);
