027fa06861fad6b7a96397a4f9f0906d5bfcc3da
Sang Le <3lesang@gmail.com>
2025-11-04 08:22:33 +0700

    update api

--- src/axios.ts
+++ src/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const axiosClient = axios.create({ baseURL: "http://localhost:8080/api/v1" });
+const axiosClient = axios.create({ baseURL: import.meta.env.VITE_API_URL });
 
 export default axiosClient;
