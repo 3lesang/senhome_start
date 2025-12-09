0908a1298cb9336649901d3e9dc4696d739200aa
Sang Le <3lesang@gmail.com>
2025-12-09 15:47:25 +0700

    update hotspot

--- src/pages/product/components/relative.tsx
+++ src/pages/product/components/relative.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { cn, convertToFileUrl, formatVND } from "@/lib/utils";
+import { convertToFileUrl, formatVND } from "@/lib/utils";
 import { getHotspotQueryOptions } from "@/queries/hotspot";
 import { PlusIcon } from "lucide-react";
 import {
@@ -8,22 +8,11 @@ import {
 	HoverCardTrigger,
 } from "@/components/ui/hover-card";
 import { Link } from "@tanstack/react-router";
-import { buttonVariants } from "@/components/ui/button";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import {
-	Carousel,
-	CarouselContent,
-	CarouselItem,
-	CarouselNext,
-	CarouselPrevious,
-} from "@/components/ui/carousel";
-import {
 	Item,
-	ItemActions,
 	ItemContent,
 	ItemDescription,
-	ItemFooter,
-	ItemHeader,
 	ItemMedia,
 	ItemTitle,
 } from "@/components/ui/item";
@@ -36,70 +25,61 @@ export function RelativeProducts({ productID }: RelativeProductsProps) {
 
 	if (!getProductsQuery.data?.length) return null;
 	return (
-		<div className="container mx-auto py-16">
-			<p className="uppercase font-bold text-2xl mb-4">Khám phá sản phẩm</p>
-			<Carousel>
-				<CarouselContent>
-					{getProductsQuery.data?.map((item) => (
-						<CarouselItem
-							key={item.id}
-							className="lg:basis-1/2 lg:max-w-[600px]"
+		<div className="max-w-[1800px] grid grid-cols-1 lg:grid-cols-3 mx-auto py-16">
+			{getProductsQuery.data?.map((item) => (
+				<div
+					key={item.id}
+					className="relative aspect-square bg-neutral-50"
+				>
+					<img
+						src={convertToFileUrl(item?.file)}
+						alt=""
+						className="object-contain w-full h-full"
+					/>
+					{item.spots.map((spot) => (
+						<div
+							key={spot.id}
+							className="absolute"
+							style={{ left: spot.x + "%", top: spot.y + "%" }}
 						>
-							<div className="relative h-[300px] lg:h-[600px] bg-neutral-50">
-								<img
-									src={convertToFileUrl(item?.file)}
-									alt=""
-									className="object-contain w-full h-full"
-								/>
-								{item.spots.map((spot) => (
-									<div
-										key={spot.id}
-										className="absolute"
-										style={{ left: spot.x + "%", top: spot.y + "%" }}
+							<HoverCard>
+								<HoverCardTrigger>
+									<button
+										type="button"
+										className="flex items-center justify-center bg-blue-500 rounded-full cursor-pointer"
 									>
-										<HoverCard>
-											<HoverCardTrigger>
-												<button
-													type="button"
-													className="flex items-center justify-center bg-blue-500 rounded-full cursor-pointer"
+										<PlusIcon className="text-white size-4" />
+									</button>
+								</HoverCardTrigger>
+								<HoverCardContent>
+									<Item>
+										<ItemMedia>
+											<Avatar>
+												<AvatarImage
+													src={convertToFileUrl(spot.product.file)}
+												/>
+											</Avatar>
+										</ItemMedia>
+										<ItemContent>
+											<ItemTitle>
+												<Link
+													to="/products/$id"
+													params={{ id: spot.product.slug }}
 												>
-													<PlusIcon className="text-white size-4" />
-												</button>
-											</HoverCardTrigger>
-											<HoverCardContent>
-												<Item>
-													<ItemMedia>
-														<Avatar>
-															<AvatarImage
-																src={convertToFileUrl(spot.product.file)}
-															/>
-														</Avatar>
-													</ItemMedia>
-													<ItemContent>
-														<ItemTitle>
-															<Link
-																to="/products/$id"
-																params={{ id: spot.product.slug }}
-															>
-																<p className="hover:underline">{spot.product.name}</p>
-															</Link>
-														</ItemTitle>
-														<ItemDescription>
-															<p>{formatVND(spot.product.sale_price)}</p>
-														</ItemDescription>
-													</ItemContent>
-												</Item>
-											</HoverCardContent>
-										</HoverCard>
-									</div>
-								))}
-							</div>
-						</CarouselItem>
+													<p className="hover:underline">{spot.product.name}</p>
+												</Link>
+											</ItemTitle>
+											<ItemDescription>
+												<p>{formatVND(spot.product.sale_price)}</p>
+											</ItemDescription>
+										</ItemContent>
+									</Item>
+								</HoverCardContent>
+							</HoverCard>
+						</div>
 					))}
-				</CarouselContent>
-				<CarouselPrevious className="hidden lg:flex" />
-				<CarouselNext className="hidden lg:flex" />
-			</Carousel>
+				</div>
+			))}
 		</div>
 	);
 }
